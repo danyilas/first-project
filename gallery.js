a75cf0df6fd3d02805ce74c9b7d7f1298c9e7bec
Chaykovskaya Daniela <cajkovskaadaniela@gmail.com>
2024-12-13 21:55:02 +0200

    Add `.gitignore` file

--- gallery.js
+++ gallery.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Инициализация слайдера с помощью Bootstrap
-    var carouselElement = document.querySelector('#carouselExampleIndicators');
-    var carousel = new bootstrap.Carousel(carouselElement, {
+    let carouselElement = document.querySelector('#carouselExampleIndicators');
+    let carousel = new bootstrap.Carousel(carouselElement, {
         interval: 3000, // Время между слайдами в миллисекундах (3 секунды)
         ride: 'carousel' // Автоматический запуск слайдера при загрузке страницы
     });
 
     // Добавление функциональности для кнопок (если нужно)
-    var nextButton = document.querySelector('.carousel-control-next');
-    var prevButton = document.querySelector('.carousel-control-prev');
+    let nextButton = document.querySelector('.carousel-control-next');
+    let prevButton = document.querySelector('.carousel-control-prev');
 
     nextButton.addEventListener('click', function () {
         carousel.next(); // Переход к следующему слайду
